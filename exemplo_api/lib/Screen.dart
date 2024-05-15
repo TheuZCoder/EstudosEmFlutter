21a46fc36ce3bf9e7b63ca48cf95970b09bbfa28
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-15 15:11:45 -0300

    att

--- exemplo_api/lib/Screen.dart
+++ exemplo_api/lib/Screen.dart
@@ -1,5 +1,5 @@
-import 'package:exemplo_api/Service.dart';
 import 'package:flutter/material.dart';
+import 'package:exemplo_api/Service.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({Key? key}) : super(key: key);
@@ -16,6 +16,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   late Map<String, dynamic> _weatherData;
   late TextEditingController _cityController;
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -24,7 +26,9 @@ class _WeatherScreenState extends State<WeatherScreen> {
     _weatherData = {
       'name': '',
       'main': {'temp': 0},
-      'weather': [{'description': ''}],
+      'weather': [
+        {'description': ''}
+      ],
     };
   }
 
@@ -35,13 +39,20 @@ class _WeatherScreenState extends State<WeatherScreen> {
   }
 
   Future<void> _fetchWeatherData(String city) async {
+    setState(() {
+      _isLoading = true;
+    });
     try {
       final weatherData = await _weatherService.getWeather(city);
       setState(() {
         _weatherData = weatherData;
+        _isLoading = false;
       });
     } catch (e) {
       print('Error fetching weather data: $e');
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
@@ -51,40 +62,54 @@ class _WeatherScreenState extends State<WeatherScreen> {
       appBar: AppBar(
         title: const Text('Previsão do Tempo'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _cityController,
-              decoration: const InputDecoration(
-                labelText: 'Nome da cidade',
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                controller: _cityController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Digite o nome da cidade';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Nome da cidade',
+                ),
               ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              final city = _cityController.text;
-              _fetchWeatherData(city);
-            },
-            child: const Text('Buscar'),
-          ),
-          const SizedBox(height: 16),
-          _weatherData == null
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('City: ${_weatherData['name']}'),
-                      Text('Temperature: ${_weatherData['main']['temp'] - 273} °C'),
-                      Text('Description: ${_weatherData['weather'][0]['description']}'),
-                    ],
-                  ),
-                ),
-        ],
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _fetchWeatherData(_cityController.text);
+                }
+              },
+              child: const Text('Buscar'),
+            ),
+            const SizedBox(height: 16),
+            _isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : _weatherData == null
+                    ? const Center(
+                        child: Text('Nenhum dado disponível'),
+                      )
+                    : Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('City: ${_weatherData['name']}'),
+                            Text('Temperature: ${(_weatherData['main']['temp'] - 273.15).toStringAsFixed(2)} °C'),
+                            Text('Description: ${_weatherData['weather'][0]['description']}'),
+                          ],
+                        ),
+                      ),
+          ],
+        ),
       ),
     );
   }
--- exemplo_api/lib/Service.dart
+++ exemplo_api/lib/Service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
-
 
 class WeatherService {
   final String apiKey;
@@ -11,6 +11,19 @@ class WeatherService {
 
   Future<Map<String, dynamic>> getWeather(String city) async {
     final url = Uri.parse('$baseUrl/weather?q=$city&appid=$apiKey');
+    final response = await http.get(url);
+    return json.decode(response.body);
+  }
+
+  Future<Map<String, dynamic>> getWeatherByLocation(
+      double lat, double lon) async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    double lat = position.latitude;
+    double lon = position.longitude;
+
+    final url = Uri.parse('$baseUrl/weather?lat=$lat&lon=$lon&appid=$apiKey');
     final response = await http.get(url);
     return json.decode(response.body);
   }
